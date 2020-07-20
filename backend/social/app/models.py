c0879e0f6c8f2b8008f029fc5cc8e180cb16f849
Vitaly Sem <virtualik@gmail.com>
2020-07-20 22:15:33 +0300

    models added

--- backend/social/app/models.py
+++ backend/social/app/models.py
@@ -1,3 +1,28 @@
+# Developed by Vitaly Sem
+
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class SocialUser(AbstractUser, models.Model):
+
+    @property
+    def fullname(self):
+        return self.get_full_name()
+
+    def __unicode__(self):
+        return self.fullname
+
+
+class Post(models.Model):
+    user = models.ForeignKey(SocialUser, related_name='posts')
+    title = models.CharField(max_length=250, unique=True)
+    added = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+
+    liked = models.IntegerField(default=0)
+    disliked = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.title
