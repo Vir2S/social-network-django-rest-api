ccd1febb64d2aca8c4267998e8e3f6a1e289cb43
Vitaly Sem <virtualik@gmail.com>
2020-07-22 02:38:27 +0300

    bot added

---
+++ backend/social/bot/exceptions.py
@@ -0,0 +1,21 @@
+# Developed by Vitaly Sem
+
+
+class BotSignUpException(Exception):
+    pass
+
+
+class BotLoginException(Exception):
+    pass
+
+
+class BotCreatePostException(Exception):
+    pass
+
+
+class BotLikeException(Exception):
+    pass
+
+
+class BotNoInitialData(Exception):
+    pass
